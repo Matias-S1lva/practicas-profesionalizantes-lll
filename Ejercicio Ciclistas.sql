dad291cfdc6b751cbf226c45001afb72f97a448f
Matias-S1lva <matiasleandro752@gmail.com>
2023-05-16 21:51:46 -0300

    se agrego el ejercicio de ciclistas trabajado en clase

---
+++ Ejercicio Ciclistas.sql
@@ -0,0 +1,84 @@
+use master 
+go
+if Exists(select * from sys.sysdatabases where name='DBCiclistas')
+drop database DBCiclistas
+go
+create Database DBCiclistas
+go
+use DBCiclistas
+go
+--creacion de tablas
+create table Ciclistas(
+ID int identity(1,1) primary key,
+Nombre varchar(60) not null,
+Nacionalidad varchar(120) not null,
+Fecha_nacimiento Date
+);
+go
+
+create table Equipos(
+ID int identity(1,1) primary key,
+Nombre varchar(60) not null,
+Nacionalidad varchar(60),
+Dir varchar(50)
+);
+go
+
+create table Contrato_ciclistas(
+FK_Ciclista int not null foreign key references Ciclistas(ID),
+FK_Equipo int not null foreign key references Equipos(ID),
+constraint ukContrato_ciclistas unique (FK_Ciclista, FK_Equipo),
+Fecha_Inicio_Contrato Date,
+Fecha_Fin_Contrato Date
+);
+
+create table Pruebas(
+ID int identity(1,1) primary key,
+Nombre varchar(60) not null,
+Año int,
+NumeroEtapa int ,
+KilometrosTotales int,
+Fk_Ciclista int not null foreign key references Ciclistas(ID),
+);
+go
+
+create table Participaciones(
+FK_Equipo int not null foreign key references Equipos(ID),
+FK_Prueba int not null foreign key references Pruebas(ID),
+constraint ukParticipaciones unique (FK_Equipo, FK_Prueba)    
+);
+go
+
+create table Nacionalidades(
+ID int identity(1,1) primary key,
+Descripcion varchar(60)
+);
+
+-- agregando Nacionalidades  
+INSERT INTO [dbo].[Nacionalidades]
+           ([Descripcion])
+     VALUES
+           ('Argentina')
+GO
+INSERT INTO [dbo].[Nacionalidades]
+           ([Descripcion])
+     VALUES
+           ('Francia')
+GO
+INSERT INTO [dbo].[Nacionalidades]
+           ([Descripcion])
+     VALUES
+           ('Brasil')
+GO
+-- agregando Ciclistas
+INSERT INTO [dbo].[Ciclistas]
+           ([Nombre]
+           ,[Nacionalidad]
+           ,[Fecha_nacimiento])
+     VALUES
+           ('Scaloni'
+           ,'Argentina'
+           ,'1987-06-24')
+GO
+
+
