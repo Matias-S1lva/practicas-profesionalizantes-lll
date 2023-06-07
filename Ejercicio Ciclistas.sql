47b86177c0826307e5f80581df67523209c73052
n1cond <n1condines@gmail.com>
2023-06-07 17:29:55 -0300

    Agregados datos en Paises, Pruebas y Ciclistas + simplificados inserts + agregado comment al inicio con pautas de formato de campos + agregado campo Edicion en Pruebas en lugar de crear una nueva tabla. Todo debería estar reflejado en el diagrama de Lucidchart.

--- Ejercicio Ciclistas.sql
+++ Ejercicio Ciclistas.sql
@@ -1,3 +1,7 @@
+--REGLAS DE FORMATO
+--Nombre de campo: Nombre_de_campo (Mayúscula_minúscula)
+--Foreign Key: Fk_Nombre_campo (Fk_Mayúscula_minúscula)
+
 use master 
 go
 if Exists(select * from sys.sysdatabases where name='DBCiclistas')
@@ -41,11 +45,12 @@ Fecha_Fin_Contrato Date
 
 create table Pruebas(
 ID int identity(1,1) primary key,
+Edicion int not null,
 Nombre varchar(60) not null,
-A�o int,
-NumeroEtapa int ,
-KilometrosTotales int,
-Fk_Ciclista int not null foreign key references Ciclistas(ID),
+Año Date,
+Nro_etapas int,
+Km_totales int,
+Fk_Ganador int foreign key references Ciclistas(ID),
 );
 go
 
@@ -57,93 +62,48 @@ constraint ukParticipaciones unique (FK_Equipo, FK_Prueba)
 go
 
 -- agregando Paises origen  
-INSERT INTO [dbo].[Paises]
-           ([Descripcion])
-     VALUES
-           ('Argentina')
-GO
 
-INSERT INTO [dbo].[Paises]
-           ([Descripcion])
-     VALUES
-           ('Francia')
-GO
-
-INSERT INTO [dbo].[Paises]
-           ([Descripcion])
-     VALUES
-           ('Brasil')
-GO
-
-INSERT INTO [dbo].[Paises]
-           ([Descripcion])
-     VALUES
-           ('Espa�a')
-GO
-
-INSERT INTO [dbo].[Paises]
-           ([Descripcion])
-     VALUES
-           ('Estados Unidos')
-GO
-
--- agregando Ciclistas
-INSERT INTO [dbo].[Ciclistas]
-           ([Nombre]
-           ,[Fk_Pais_origen]
-           ,[Fecha_nacimiento])
-     VALUES
-           ('Scaloni'
-           ,1
-           ,'1987-06-24')
-GO
+INSERT INTO Paises VALUES ('Argentina'); --1
+INSERT INTO Paises VALUES ('Francia');
+INSERT INTO Paises VALUES ('Brasil');
+INSERT INTO Paises VALUES ('España');
+INSERT INTO Paises VALUES ('Estados Unidos'); --5
+INSERT INTO Paises VALUES ('Colombia');
+INSERT INTO Paises VALUES ('Dinamarca');
+INSERT INTO Paises VALUES ('Países Bajos');
+INSERT INTO Paises VALUES ('Alemania');
+INSERT INTO Paises VALUES ('Yugoslavia'); --10
+INSERT INTO Paises VALUES ('Bélgica');
+INSERT INTO Paises VALUES ('Irlanda');
+INSERT INTO Paises VALUES ('Gran Bretaña');
 
 -- agregando Equipos
-INSERT INTO [dbo].[Equipos]
-			([Nombre],
-			[Fk_Pais_origen],
-			[Director])
-	VALUES
-		('Movistar Team',
-		4,
-		'Alfonso Galilea')
-GO
-
-INSERT INTO [dbo].[Equipos]
-			([Nombre],
-			[Fk_Pais_origen],
-			[Director])
-	VALUES
-		('Trek-Segafredo',
-		5,
-		'Steven de Jongh')
-GO
-
-INSERT INTO [dbo].[Equipos]
-			([Nombre],
-			[Fk_Pais_origen],
-			[Director])
-	VALUES
-		('Weber Shimano Ladies Power',
-		1,
-		'Veronica Martinez')
-GO
-
-INSERT INTO [dbo].[Equipos]
-			([Nombre],
-			[Fk_Pais_origen],
-			[Director])
-	VALUES
-		('Soul Brasil Pro Cycling Team',
-		3,
-		'Ana Paula Castro')
-GO
+INSERT INTO Equipos VALUES ('Movistar Team', 4, 'Alfonso Galilea'); --1
+INSERT INTO Equipos VALUES ('Trek-Segafredo', 5, 'Steven de Jongh');
+INSERT INTO Equipos VALUES ('Weber Shimano Ladies Power', 1, 'Veronica Martinez');
+INSERT INTO Equipos VALUES ('Soul Brasil Pro Cycling Team', 3, 'Ana Paula Castro');
+INSERT INTO Equipos VALUES ('Jumbo-Visma', 8, 'Addy Engels'); --5
+INSERT INTO Equipos VALUES ('Bora-Hansgrohe', 9, 'Rolf Aldag'); 
+INSERT INTO Equipos VALUES ('INEOS Grenadiers', 13, 'Shane Sutton');
 
 
---FORMATO DE FECHA: AAAA-MM-DD para que funcione en otra versi�n de SQL
-insert into Ciclistas values ('Roberto Perez',1, '2000-09-13');
-insert into Ciclistas values ('Pablo Garcia',2, '1992-02-23');
-insert into Ciclistas values ('Jorge Docampo',1, '1942-04-17');
-insert into Ciclistas values ('Lionel Scaloni',1, '1962-01-09');
-insert into Ciclistas values ('Lionel Marconi',3, '1962-09-13');
+--FORMATO DE FECHA: AAAA-MM-DD para que funcione en otra versión de SQL
+insert into Ciclistas values ('Roberto Perez', 1, '2000-09-13'); --1
+insert into Ciclistas values ('Pablo Garcia', 2, '1992-02-23');
+insert into Ciclistas values ('Jorge Docampo', 1, '1942-04-17');
+insert into Ciclistas values ('Lionel Scaloni', 1, '1962-01-09');
+insert into Ciclistas values ('Lionel Marconi', 3, '1962-09-13'); --5
+insert into Ciclistas values ('Sergio Higuita', 6, '1997-08-01');
+insert into Ciclistas values ('Jonas Vingegaard', 7, '1996-12-10');
+insert into Ciclistas values ('Primož Roglič', 10, '1989-10-29');
+insert into Ciclistas values ('Sam Bennett', 11, '1990-10-16');
+insert into Ciclistas values ('Geraint Thomas', 13, '1986-05-25'); --10
+
+--Agregando Pruebas
+insert into Pruebas (Edicion, Nombre, Año, Nro_etapas, Km_totales, Fk_Ganador) 
+	values (109,'Tour de France', '2022', 21, 3350, 7);
+insert into Pruebas values (101, 'Volta a Cataluña', '2022', 7, 1213, 6);
+insert into Pruebas values (88, 'Paris-Niza', '2022', 8, 1197, 8);
+insert into Pruebas values (60, 'Eschborn-Frankfurt der Radklassiker', '2022', 1, 185, 9);
+insert into Pruebas values (85, 'Vuelta a Suiza', '2022', 8, 1340, 10);
 
